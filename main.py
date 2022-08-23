363bc2e03d9abd0a13d5fced829bb1823036adba
noah-schechter <noah.schechter1@gmail.com>
2022-08-23 00:03:36 -0700

    Reduce bloat

--- main.py
+++ main.py
@@ -16,22 +16,8 @@ table_2_name = 'tblcgPnwDpkj6x36D'
 recipes_table = Table(api_key, base_key, table_2_name)
 
 """
-Accesses Airtable and returns list of available drinks
+Accessed Airtable and returns dict of in-stock drinks and specific ingredients.
 """
-def fetchInventory():
-    records = inventory_table.all()
-    inventory = {}
-    for row in records:
-        fields = row['fields']
-        typeDrink = fields['Type of Alcohol']
-        ingredient = fields['Product']
-        if typeDrink not in inventory:
-            inventory[typeDrink] = []
-        inventory[typeDrink].append(ingredient)
-    return inventory
-
-
-
 def fetchRecipes():
     records = recipes_table.all()
     recipes = {}
@@ -52,28 +38,6 @@ def fetchRecipes():
                     ingredientsProcessed.append(specialRaw.strip())
             recipes[drink] = ingredientsProcessed
     return recipes
-
-"""
-Takes in recipe of form [ingredient list] and inventory of form {type of drink: [products], type of drink: [products]. Returns boolean indicating whether a drink can be made, and, if true, a list of products in barcart that comprise that recipe.
-"""
-def constructMenuItem(ingredients, inventory):
-    menuItem = []
-    for ingredient in ingredients:
-        if ingredient not in inventory:
-            return False, []
-        menuItem.append(inventory[ingredient][0]) #this is where we could break ties in a different way
-    return True, menuItem
-
-"""
-Loops through recipes and constructs a menu of drinks that can be assembled, and which products they will require.
-"""
-def constructMenu(recipes, inventory):
-    menu = {}
-    for drink in recipes.keys():
-        possible, ingredients = constructMenuItem(recipes[drink], inventory)
-        if possible and drink not in menu:
-            menu[drink] = ingredients
-    return menu
 
 
 @app.get("/")
